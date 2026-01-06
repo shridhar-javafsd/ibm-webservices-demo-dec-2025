51eaa84b8eb69adf5e927ded95b10b54b7298c39
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-06 15:35:09 +0530

    asdf

--- consume-apis-demo/index.js
+++ consume-apis-demo/index.js
@@ -1,5 +1,4 @@
 
-
 const restApiUrl = 'http://localhost:3000/employees';
 const soapApiUrl = 'http://localhost:4000/employee';
 const graphqlApiUrl = 'http://localhost:5000/graphql';
@@ -16,45 +15,6 @@ const getAllEmployeesRest = async () => {
 
 const getAllEmployeesSoap = async () => {
 
-    
-};
-
-
-const getAllEmployeesGraphql = async () => {
-
-    
-};
-
-getAllEmployeesRest();
-getAllEmployeesSoap();
-getAllEmployeesGraphql();
-
-
-
-
-
-
-
-
-
-
-
-
-const restApiUrl = 'http://localhost:3000/employees';
-const soapApiUrl = 'http://localhost:4000/employee';
-const graphqlApiUrl = 'http://localhost:5000/graphql';
-
-const getAllEmployeesRest = async () => {
-    try {
-        const response = await fetch(restApiUrl);
-        const employees = await response.json();
-        console.log('REST Employees:', employees);
-    } catch (err) {
-        console.error('REST Error:', err);
-    }
-};
-
-const getAllEmployeesSoap = async () => {
     try {
         const soapRequest = `
             <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
@@ -67,7 +27,6 @@ const getAllEmployeesSoap = async () => {
                </soapenv:Body>
             </soapenv:Envelope>
         `;
-
         const response = await fetch(soapApiUrl, {
             method: 'POST',
             headers: {
@@ -75,48 +34,132 @@ const getAllEmployeesSoap = async () => {
             },
             body: soapRequest
         });
+        const employees = await response.text();
+        console.log(employees);
 
-        const textResponse = await response.text();
-        console.log('SOAP Employee (XML):', textResponse);
-
-    } catch (err) {
-        console.error('SOAP Error:', err);
     }
+    catch (err) {
+        console.error(err);
+    }
+
 };
 
 
 const getAllEmployeesGraphql = async () => {
-    try {
-        const query = {
-            query: `
-                query {
-                    employees {
-                        id
-                        name
-                        salary
-                    }
-                }
-            `
-        };
 
-        const response = await fetch(graphqlApiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(query)
-        });
 
-        const result = await response.json();
-        console.log('GraphQL Employees:', result.data.employees);
-
-    } catch (err) {
-        console.error('GraphQL Error:', err);
-    }
 };
-
 
 getAllEmployeesRest();
 getAllEmployeesSoap();
 getAllEmployeesGraphql();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const restApiUrl = 'http://localhost:3000/employees';
+// const soapApiUrl = 'http://localhost:4000/employee';
+// const graphqlApiUrl = 'http://localhost:5000/graphql';
+
+// const getAllEmployeesRest = async () => {
+//     try {
+//         const response = await fetch(restApiUrl);
+//         const employees = await response.json();
+//         console.log('REST Employees:', employees);
+//     } catch (err) {
+//         console.error('REST Error:', err);
+//     }
+// };
+
+// const getAllEmployeesSoap = async () => {
+//     try {
+//         const soapRequest = `
+//             <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+//                               xmlns:emp="http://example.com/employee">
+//                <soapenv:Header/>
+//                <soapenv:Body>
+//                   <emp:getEmployeeRequest>
+//                      <emp:id>101</emp:id>
+//                   </emp:getEmployeeRequest>
+//                </soapenv:Body>
+//             </soapenv:Envelope>
+//         `;
+
+//         const response = await fetch(soapApiUrl, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'text/xml'
+//             },
+//             body: soapRequest
+//         });
+
+//         const textResponse = await response.text();
+//         console.log('SOAP Employee (XML):', textResponse);
+
+//     } catch (err) {
+//         console.error('SOAP Error:', err);
+//     }
+// };
+
+
+// const getAllEmployeesGraphql = async () => {
+//     try {
+//         const query = {
+//             query: `
+//                 query {
+//                     employees {
+//                         id
+//                         name
+//                         salary
+//                     }
+//                 }
+//             `
+//         };
+
+//         const response = await fetch(graphqlApiUrl, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(query)
+//         });
+
+//         const result = await response.json();
+//         console.log('GraphQL Employees:', result.data.employees);
+
+//     } catch (err) {
+//         console.error('GraphQL Error:', err);
+//     }
+// };
+
+
+// getAllEmployeesRest();
+// getAllEmployeesSoap();
+// getAllEmployeesGraphql();
