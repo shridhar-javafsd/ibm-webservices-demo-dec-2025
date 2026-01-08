755c048a8193efccb8bf998335982935661cda28
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-08 15:10:45 +0530

    html fundas

--- frontend-demos/html-css-demos/forms.js
+++ frontend-demos/html-css-demos/forms.js
@@ -1,7 +1,24 @@
+console.log('forms data');
 
 // print form data on console after submission 
 
-const printFormData = () => {
-    const formData = '';
+const printFormData = (event, form) => {
+    // event.preventDefault();
+    const formData = Object.fromEntries(new FormData(form));
     console.log(formData);
 };
+
+// const printFormData = (event) => {
+//     event.preventDefault();
+//     const formData = {
+//         name: document.getElementById("name").value,
+//         username: document.getElementById("username").value,
+//         email: document.getElementById("email").value,
+//         phone: document.getElementById("phone").value,
+//         aadhaar: document.getElementById("aadhaar").value,
+//         password: document.getElementById("password").value
+//     };
+//     console.log(formData);
+// };
+
+
