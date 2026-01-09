3934af424cf8ebcf5b3aa71bba702ef0c85032c6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-09 11:43:56 +0530

    JavaScript recap

--- frontend-demos/js-reacp-demos/script.js
+++ frontend-demos/js-reacp-demos/script.js
@@ -1,1 +1,20 @@
 
+// consume REST APIs 
+
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/users/';
+let userId = 0;
+
+const fetchUserData = () => {
+    
+    fetch(`${apiUrl}${userId}`)
+        .then((response) => {
+            console.log(response);
+            return response.json()
+        })
+        .then((response) => {
+            console.log(response);
+            document.getElementById("username").innerText = response.username;
+        })
+        .catch();
+};
