05e3cdf7c9842e7e044535bcdc5f5db3102154fc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-09 15:25:35 +0530

    javascript recap

--- frontend-demos/js-reacp-demos/js-recap.js
+++ frontend-demos/js-reacp-demos/js-recap.js
@@ -1,58 +1,85 @@
-// // rest and spread in JS 
+// // // rest and spread in JS 
 
-// // // rest 
+// // // // rest 
 
-// // const nums = (...abc) => {
-// //     console.log(abc);
-// //     // code 
-// // };
+// // // const nums = (...abc) => {
+// // //     console.log(abc);
+// // //     // code 
+// // // };
 
-// // nums(10);
-// // nums(10, 20);
-// // nums(10, 20, 30);
+// // // nums(10);
+// // // nums(10, 20);
+// // // nums(10, 20, 30);
 
-// // spread 
+// // // spread 
+
+// // // const nums = [10, 20, 30];
+// // // const moreNums = [...nums, 40, 50];
+// // // console.log(moreNums);
+
+// // // const nums2 = [10, 20, 30, 40, 50];
+// // // const [ten, twenty, ...remaining] = nums2; // rest or spread 
+// // // console.log(remaining);
+
+// // const userData = { id: 101, name: 'Sonu', salary: 10.25 };
+// // const moreData = { phone: 986543210, email: 'sonu@gmail.com' };
+
+// // const fullData = { ...userData, ...moreData };
+// // console.log(fullData);
+
+// // object destructuring in js ^ 
+
+// // 
+
+// // keys, values of object and object array 
+
+// const employees = [
+//     { id: 101, name: 'Sonu', salary: 10.50 },
+//     { id: 102, name: 'Monu', salary: 11.75 },
+//     { id: 103, name: 'Tonu', salary: 14.25 },
+//     { id: 104, name: 'Ponu', salary: 15.25 },
+//     { id: 105, name: 'Gonu', salary: 13.50 }
+// ];
+
+// // // employees.forEach
+// // employees.forEach(e => console.log(e));
+// // // employees.filter
+// // employees.filter(e => e.salary > 11).forEach(e => console.log(e));
+// // // employees.map
+// // employees.map(e => ({
+// //     ...e,
+// //     salary: e.salary * 1.10
+// // })).forEach(e => console.log(e));
+// // employees.reduce
+// // console.log(employees.reduce((sum, e) => sum + e.salary, 0));
+// // // employees.slice
+// // employees.slice(0, 2).forEach(e => console.log(e));
+
+// // splicedEmployees = employees.splice(1, 3);
+
+// // splicedEmployees.forEach(e => console.log(e));
+
+// // employees.forEach(e => console.log(e));
+
+// // // employees.keys
+// // // employees.values
+// // // employees.entries
+// // // employees.sort()
+// // // find
+// // // push 
+// // // pop 
 
 // // const nums = [10, 20, 30];
-// // const moreNums = [...nums, 40, 50];
-// // console.log(moreNums);
-
-// // const nums2 = [10, 20, 30, 40, 50];
-// // const [ten, twenty, ...remaining] = nums2; // rest or spread 
-// // console.log(remaining);
-
-// const userData = { id: 101, name: 'Sonu', salary: 10.25 };
-// const moreData = { phone: 986543210, email: 'sonu@gmail.com' };
-
-// const fullData = { ...userData, ...moreData };
-// console.log(fullData);
-
-// object destructuring in js ^ 
-
-// 
-
-// keys, values of object and object array 
-
-const employees = [
-    { id: 101, name: 'Sonu', salary: 10.50 },
-    { id: 102, name: 'Monu', salary: 11.75 },
-    { id: 103, name: 'Tonu', salary: 14.25 },
-    { id: 104, name: 'Ponu', salary: 15.25 },
-    { id: 105, name: 'Gonu', salary: 13.50 }
-];
-
-// // employees.forEach
-// employees.forEach(e => console.log(e));
-// // employees.filter
-// employees.filter(e => e.salary > 11).forEach(e => console.log(e));
-// // employees.map
-// employees.map(e => ({
-//     ...e,
-//     salary: e.salary * 1.10
-// })).forEach(e => console.log(e));
-// employees.reduce
-// console.log(employees.reduce((sum, e) => sum + e.salary, 0));
-// // employees.slice
-// employees.slice(0, 2).forEach(e => console.log(e));
+// // nums[9] = 100;
+// // console.log(nums[5]);
+// // nums[7] = 70;
 
 
+// // object methods - 
+
+
+
+const employee = { id: 101, name: 'Sonu', salary: 10.50 };
+
+Object.freeze(employee);
+Object.
