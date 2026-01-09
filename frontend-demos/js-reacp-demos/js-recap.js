be29759e65fbe33671e201d7ab03de48de9dc719
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-09 14:35:23 +0530

    javascript recap

---
+++ frontend-demos/js-reacp-demos/js-recap.js
@@ -0,0 +1,24 @@
+// rest and spread in JS 
+
+// // rest 
+
+// const nums = (...args) => {
+//     console.log(args);
+//     // code 
+// };
+
+// nums(10);
+// nums(10, 20);
+// nums(10, 20, 30);
+
+// spread 
+
+const nums = [10, 20, 30];
+const moreNums = [...nums, 40, 50];
+console.log(moreNums);
+
+
+
+
+
+
