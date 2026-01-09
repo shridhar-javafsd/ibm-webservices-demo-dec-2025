f656b8660cd049622d4605c1fc5bb5009e47848a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-09 14:46:25 +0530

    javascript recap

--- frontend-demos/js-reacp-demos/js-recap.js
+++ frontend-demos/js-reacp-demos/js-recap.js
@@ -2,8 +2,8 @@
 
 // // rest 
 
-// const nums = (...args) => {
-//     console.log(args);
+// const nums = (...abc) => {
+//     console.log(abc);
 //     // code 
 // };
 
@@ -13,12 +13,17 @@
 
 // spread 
 
-const nums = [10, 20, 30];
-const moreNums = [...nums, 40, 50];
-console.log(moreNums);
+// const nums = [10, 20, 30];
+// const moreNums = [...nums, 40, 50];
+// console.log(moreNums);
 
+// const nums2 = [10, 20, 30, 40, 50];
+// const [ten, twenty, ...remaining] = nums2; // rest or spread 
+// console.log(remaining);
 
+const userData = { id: 101, name: 'Sonu', salary: 10.25 };
+const moreData = { phone: 986543210, email: 'sonu@gmail.com' };
 
-
-
+const fullData = { ...userData, ...moreData };
+console.log(fullData);
 
