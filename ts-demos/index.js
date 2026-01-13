ad70b59525304a0470916fd236aa5ef5708cbf31
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-13 09:58:23 +0530

    typescript demos

--- ts-demos/index.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello world!");
-var num = 10;
-console.log(num);
--- ts-demos/index.ts
+++ ts-demos/index.ts
@@ -1,6 +1,33 @@
 console.log("Hello world!");
 
-const num: number = 10;
-console.log(num);
 
-// npm install --location=global @angular/cli
+
+// // TypeScript 
+
+// // JavaScript is dynamically types language == not typesafe 
+
+// // Java - typesafe language 
+
+// // double salary = 10.50; // 
+
+// let salary = 10.50;
+// console.log('My salary is: ', salary);
+// salary = 12.25;
+// console.log('My salary is: ', salary);
+// salary = false;
+// console.log('My salary is: ', salary);
+
+
+
+// // npm install --location=global typescript 
+
+// // write code in .ts file 
+// // transpile it with - 
+// // tsc filename.ts 
+// // gets - 
+// // filename.js 
+// // run - 
+// // node filename.js 
+
+
+
