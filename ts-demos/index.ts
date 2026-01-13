355449a6a2317c3c70bb7bbf8ebbbc2d56e77254
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-01-13 10:41:58 +0530

    typescript demos

--- ts-demos/index.ts
+++ ts-demos/index.ts
@@ -25,6 +25,16 @@
 //     console.log(a + b);
 // }
 
+// // default params  
+// const addNums = (a: number, b: number = 10): void  => {
+//     console.log(a + b);
+// }
+
+// // optional params  
+// const addNums = (a: number, b?: number = 10): void  => {
+//     console.log(a + b);
+// }
+
 // // another example 
 // const addNums = (a: number, b: number): number => {
 //     return a + b;
@@ -34,11 +44,44 @@
 // const values: number[] = [10, 20, 30, 40, 50];
 // values[0] = false;
 
-// 5. object properties -
+// // 5. object properties -
+// const employee: {
+//     id: number;
+//     name: string;
+//     salary: number;
+//     isPermanent: boolean;
+// } = {
+//     id: 101,
+//     name: "Sonu",
+//     salary: 50000,
+//     isPermanent: true
+// };
 
-const employee = {
-    // complete 
-}
+// // 6. union types
+// let id: number | string;
+// id = 101;
+// id = 'Sonu';
+
+// let PORT = comingFromSomeSource() | 2000;
+
+
+// 7. type alias
+type Employee = {
+    id: number;
+    name: string;
+    salary: number;
+};
+
+const emp1: Employee = {
+    id: 102,
+    name: 'Monu',
+    salary: 12.50
+};
+
+// // other topics  - any, unknown, tuples, generics, interfaces, enums,
+// // class constructors and methods etc
+// // decorators, Access modifiers
+
 
 // // TypeScript
 
